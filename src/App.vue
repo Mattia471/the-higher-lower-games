86a4eb053e3d34a6a41ab5415ff5a38808efca85
Mattia471 <cucuzzamattia47@gmail.com>
2023-04-14 17:16:05 +0200

    defined structure + component reusable

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+import BoxComponent from "@/components/BoxComponent.vue";</script>
+
+<template>
+  <div class="container-full">
+    <!--LEFT-->
+    <BoxComponent classContainer="container-left" title-box="JAVA">
+      <template #content>
+        <p class="text-follower-center counter">200,565</p>
+      </template>
+      <template #scoreDescription>
+        <p class="text-bottom-left">High Score: 0</p>
+      </template>
+    </BoxComponent>
+    <!--CENTER-->
+    <div class="circle"><p>VS</p></div>
+    <div class="rectangle"><p>Which topic is the most searched for?</p></div>
+    <!--RIGHT-->
+    <BoxComponent classContainer="container-right" title-box="PHP">
+      <template #content>
+        <div>
+          <button class="button">Higher</button>
+          <button class="button">Lower</button>
+        </div>
+      </template>
+      <template #scoreDescription>
+        <p class="text-bottom-right">Score: 0</p>
+      </template>
+    </BoxComponent>
+  </div>
+</template>
---
+++ src/components/BoxComponent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+//crea una props
+const props = defineProps<{
+  classContainer: string,
+  titleBox: string
+}>()
+</script>
+<template>
+  <div :class="classContainer">
+    <div class="sub-container-full">
+      <p class="text-follower-center-up">{{ titleBox }}</p>
+      <slot name="content"></slot>
+    </div>
+    <slot name="scoreDescription"></slot>
+  </div>
+</template>
